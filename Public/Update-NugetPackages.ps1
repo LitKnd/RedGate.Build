fcf0d1cdebf91bc4a0de42a2254a5a5b288603e9
Ally Parker <Ally.Parker@red-gate.com>
2017-03-08 12:56:59 +0000

    Add Nuget Update Helpers

---
+++ Public/Update-NugetPackages.ps1
@@ -0,0 +1,29 @@
+<#
+        .SYNOPSIS
+        Updates a solution with the NuGet Packages IDs supplied
+        .DESCRIPTION
+        Updates a solution with the NuGet Packages IDs supplied        
+        .PARAMETER Solution
+        The name of the .sln file
+        .PARAMETER packageIds
+        An array of nuget Package IDs to update
+#>
+#requires -Version 2
+function Update-NugetPackages
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Solution,
+        
+        [Parameter(Mandatory = $true)]
+        [string[]] $packageIds
+    )
+    $NugetPackageParam = packageIds | ForEach-Object {
+        "-id", $_
+    }
+
+    exec {
+        & $NugetExe update $Solution -Verbosity detailed -noninteractive $NugetPackageParam
+    }
+}
