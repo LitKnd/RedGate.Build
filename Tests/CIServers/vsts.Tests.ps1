a18d44d446bbf396945bc00531bd256341ccf1b4
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-11-21 16:14:57 +0000

    Use BeforeAll / AfterAll for the tests

--- Tests/CIServers/vsts.Tests.ps1
+++ Tests/CIServers/vsts.Tests.ps1
@@ -1,13 +1,20 @@
 #requires -Version 4 -Modules Pester
 
 Describe 'Write-VSTSLoggingCommand' {
+    BeforeAll {
+        # Redirect Write-Host to Write-Output so that we can capture it and check it.
+        function global:Redirect-HostToOutput-ForTest { Write-Output $Args[0] }
+        New-Alias -Name Write-Host -Value Redirect-HostToOutput-ForTest -Force -Scope Global
+    }
 
-    # Redirect Write-Host to Write-Output so that we can capture it and check it.
-    function global:Redirect-HostToOutput-ForTest { Write-Output $Args[0] }
-    New-Alias -Name Write-Host -Value Redirect-HostToOutput-ForTest -Force -Scope Global
+    AfterAll {
+        # Cleanup the alias we created for the tests
+        Remove-Item alias:\Write-Host -Force
+    }
+
 
     It 'should handle missing -Properties' {
-        Write-VSTSLoggingCommand 'area.action' 'message' | Should Be '##vso[area.action ]message'
+        Write-VSTSLoggingCommand 'area.action' 'message' | Should Be '##vso[area.action]message'
     }
 
     It 'should pass -Properties properly' {
@@ -17,6 +24,4 @@ Describe 'Write-VSTSLoggingCommand' {
         } | Should Be '##vso[area.properties prop2=value2;prop1=value1]this is a message'
     }
 
-    # Cleanup the alias we created for the tests
-    Remove-Item alias:\Write-Host -Force
 }
