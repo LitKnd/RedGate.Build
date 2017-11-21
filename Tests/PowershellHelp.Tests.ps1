6faa720f37d9d921cb35ea5993c3b203c73ddef6
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-11-21 16:59:57 +0000

    We don't really care about the help for the functions that print CI related messages to the output

--- Tests/PowershellHelp.Tests.ps1
+++ Tests/PowershellHelp.Tests.ps1
@@ -6,8 +6,9 @@ Describe 'All exported functions help should be defined' {
 
   Get-Command -Module RedGate.Build | where {
       $_.Name -notlike '*TeamCity*' `
-      -and $_.Name -notlike 'Write-CI*' `
-      -and $_.Name -notlike 'Write-VSTS*'
+      -and $_.Name -notlike 'CI-*' `
+      -and $_.Name -notlike '*-CI*' `
+      -and $_.Name -notlike '*VSTS*'
     } | ForEach {
     $command = $_
     Context "Command $($command.Name)" {
