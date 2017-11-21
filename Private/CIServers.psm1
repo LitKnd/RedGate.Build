3f0f2ab89578167365310599a67fb3567525c6de
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-11-21 11:53:32 +0000

    Introduce helper variable to quickly determine if we are running a build from Teamcity or VSTS

--- Private/CIServers.psm1
+++ Private/CIServers.psm1
@@ -1,7 +1,15 @@
 
+if($env:TEAMCITY_VERSION) {
+    $CIServer = 'Teamcity'
+} elseif($env:TF_BUILD) {
+    $CIServer = 'VSTS'
+} else {
+    $CIServer = 'Unknown'
+}
+
 . $PSScriptRoot\CIServers\teamcity.ps1
 . $PSScriptRoot\CIServers\vsts.ps1
 
 
 
-Export-ModuleMember -Function * -Alias *
+Export-ModuleMember -Function * -Alias * -Variable CIServer
