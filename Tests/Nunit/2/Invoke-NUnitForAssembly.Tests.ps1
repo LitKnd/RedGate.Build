1c504535f7827c6a4b8c98442697240d795dfc25
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-11-22 14:54:32 +0000

    Rewrote a test so that "C:\Program Files (x86)\MSBuild\14.0\Bin\csc.exe" is not required to build.

--- Tests/Nunit/2/Invoke-NUnitForAssembly.Tests.ps1
+++ Tests/Nunit/2/Invoke-NUnitForAssembly.Tests.ps1
@@ -4,8 +4,6 @@ $FullPathToModuleRoot = Resolve-Path $PSScriptRoot\..\..\..
 
 Describe 'Invoke-NUnitForAssembly' {
 
-    $tempFolder = New-Item "$FullPathToModuleRoot\.temp\nunit" -ItemType Directory -Force -Verbose
-
     Context 'Nunit 4.0' {
         It "should throw Unexpected NUnit version" {
             { Invoke-NUnitForAssembly -Assembly 'myassembly.dll' -NUnitVersion '4.0' } | Should Throw "Unexpected NUnit version '4.0'. This function only supports Nunit v2"
@@ -18,35 +16,17 @@ Describe 'Invoke-NUnitForAssembly' {
         }
     }
 
-    function Compile-NUnitTests() {
-        Install-Package -Name NUnit.Runners -Version 2.6.4
-        $nunit = "$FullPathToModuleRoot\packages\NUnit.Runners.2.6.4"
-        @'
-using NUnit.Framework;
-
-namespace Tests
-{
-    [TestFixture]
-    public class SampleTests
-    {
-        [Test] public void AlwaysGreen() {}
-    }
-}
-'@  | Out-File $tempFolder\nunit-test.cs
-
-        Copy-Item $nunit\tools\nunit.framework.dll -Destination $tempFolder
-        $compiler = 'C:\Program Files (x86)\MSBuild\14.0\Bin\csc.exe'
-        $compiler | Should Exist
-        & $compiler /target:library /out:$tempFolder\nunit-test.dll $tempFolder\nunit-test.cs /reference:$tempFolder\nunit.framework.dll 2>&1 > $tempFolder\nunit-test.out.txt
-        $LASTEXITCODE | Should Be 0
-    }
-
     Context 'Run real NUnit tests' {
-        Compile-NUnitTests
+        $tempFolder = New-Item "$FullPathToModuleRoot\.temp\nunit" -ItemType Directory -Force -Verbose
+        # nunit2-test.dll is a NUnit assembly (compiled against NUnit 2.6.4) with a single test that will always pass.
+        Copy-Item $PSScriptRoot\nunit2-test.dll -Destination $tempFolder -Verbose
+
+        Install-Package -Name NUnit.Runners -Version 2.6.4
+        Copy-Item $FullPathToModuleRoot\packages\NUnit.Runners.2.6.4\tools\nunit.framework.dll -Destination $tempFolder -Verbose
 
         It 'Should not throw exceptions when TestResultFilenamePattern is empty' {
             Invoke-NUnitForAssembly `
-                -AssemblyPath $tempFolder\nunit-test.dll `
+                -AssemblyPath $tempFolder\nunit2-test.dll `
                 -NUnitVersion '2.6.4' `
                 -TestResultFilenamePattern $null
         }
