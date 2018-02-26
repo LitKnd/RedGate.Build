1a705ce822aba0b4d54e3c49b27a04859572c9af
Sam Blackburn <sam.blackburn@red-gate.com>
2018-02-26 13:16:26 +0000

    Red: Auto-Merge should not go boomy

---
+++ Tests/Tests/Test-AutoMerge.ps1
@@ -0,0 +1,11 @@
+#requires -Version 4 -Modules Pester
+
+Describe 'Test-AutoMerge' {
+
+    Context 'When there are no PRs to merge' {
+
+        It 'should not throw any exception' {
+            { Merge-PullRequest -GithubApiToken 'fake-token' -Repo 'nonexistent-repo' -Branch 'nonexistent-branch' -Revision 'nonexistent-revision' } | Should Not Throw
+        }
+    }
+}
