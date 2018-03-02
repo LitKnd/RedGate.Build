e5e05fc90e5e5a93be04a2ced6a1abdf35e60084
Asha Patel <asha.patel@red-gate.com>
2018-03-02 12:09:26 +0000

    Alter Install-NUnitPackage.ps1 so we pick up the correct nunit runner when working on different nunit versions

--- Public/Nuget/Install-NUnitPackage.ps1
+++ Public/Nuget/Install-NUnitPackage.ps1
@@ -1,14 +1,21 @@
 <#
 .SYNOPSIS
-  Install NUnit.Runners to RedGate.Build\packages
+  Installs either NUnit.Runners, NUnit.Console or NUnit.ConsoleRunner to RedGate.Build\packages depending on the nunit version
 .DESCRIPTION
-  Install the NUnit.Runners nuget package to RedGate.Build\packages
+  Install either NUnit.Runners, NUnit.Console or NUnit.ConsoleRunner to RedGate.Build\packages depending on the nunit version
 #>
 function Install-NUnitPackage {
   [CmdletBinding()]
   param(
-    # The version of the nuget package containing the NUnit executables (NUnit.Runners)
+    # The version of the nuget package containing the NUnit executables (NUnit.Runners (v < 3), NUnit.Console (3.0 <= v <= 3.1) or NUnit.ConsoleRunner (v > 3.1))
     [string] $Version = $DefaultNUnitVersion
   )
-  Install-Package "NUnit.Runners" $Version
+    $nunitRunnerPackageName = "NUnit.ConsoleRunner"
+    if ($Version.StartsWith("3.0") -or $Version.StartsWith("3.1")) {
+      $nunitRunnerPackageName = "NUnit.Console"
+    }
+	elseif ($Version.StartsWith("0.") -or $Version.StartsWith("1.") -or $Version.StartsWith("2.")){
+      $nunitRunnerPackageName = "NUnit.Runners"
+    }
+  Install-Package "$nunitRunnerPackageName" $Version
 }
