7a1ccda94f7e067258cecc13bba26621ae1c3640
Asha Patel <asha.patel@red-gate.com>
2018-03-05 11:07:09 +0000

    Actually need package Nunit.Console, not Nunit.ConsoleRunner, for v >= 3.0

--- Public/Nuget/Install-NUnitPackage.ps1
+++ Public/Nuget/Install-NUnitPackage.ps1
@@ -1,20 +1,17 @@
 <#
 .SYNOPSIS
-  Installs either NUnit.Runners, NUnit.Console or NUnit.ConsoleRunner to RedGate.Build\packages depending on the nunit version
+  Installs either NUnit.Runners or NUnit.Console to RedGate.Build\packages depending on the nunit version
 .DESCRIPTION
-  Install either NUnit.Runners, NUnit.Console or NUnit.ConsoleRunner to RedGate.Build\packages depending on the nunit version
+  Install either NUnit.Runners or NUnit.Console to RedGate.Build\packages depending on the nunit version
 #>
 function Install-NUnitPackage {
   [CmdletBinding()]
   param(
-    # The version of the nuget package containing the NUnit executables (NUnit.Runners (v < 3), NUnit.Console (3.0 <= v <= 3.1) or NUnit.ConsoleRunner (v > 3.1))
+    # The version of the nuget package containing the NUnit executables (NUnit.Runners (v < 3) or NUnit.Console (v >= 3)
     [string] $Version = $DefaultNUnitVersion
   )
-    $nunitRunnerPackageName = "NUnit.ConsoleRunner"
-    if ($Version.StartsWith("3.0") -or $Version.StartsWith("3.1")) {
-      $nunitRunnerPackageName = "NUnit.Console"
-    }
-	elseif ($Version.StartsWith("0.") -or $Version.StartsWith("1.") -or $Version.StartsWith("2.")){
+    $nunitRunnerPackageName = "NUnit.Console"
+    if ($Version.StartsWith("2.")){
       $nunitRunnerPackageName = "NUnit.Runners"
     }
   Install-Package "$nunitRunnerPackageName" $Version
