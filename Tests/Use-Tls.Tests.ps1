5bcbafafd6fa242cfe0e0bcfe623a47be6484ad7
Sam Blackburn <sam.blackburn@red-gate.com>
2018-03-23 16:16:56 +0000

    Typo in assertion message

--- Tests/Use-Tls.Tests.ps1
+++ Tests/Use-Tls.Tests.ps1
@@ -2,7 +2,7 @@
 
 Describe 'Use-Tls' {
     Context 'When given a delegate that throws' {
-        It 'should cause the exception to be throw' {
+        It 'should cause the exception to be thrown' {
             { Use-Tls { throw "test error message"; } } | Should -Throw "test error message"
         }
     }
