e2975de911999e5c9e6f3cdb3bb23356519651d4
Sam Blackburn <sam.blackburn@red-gate.com>
2018-03-23 15:59:43 +0000

    Add a test to ensure the delegate actually runs

---
+++ Tests/Use-Tls.Tests.ps1
@@ -0,0 +1,9 @@
+#requires -Version 2 -Modules Pester
+
+Describe 'Use-Tls' {
+    Context 'When given a delegate that throws' {
+        It 'should cause the exception to be throw' {
+            { Use-Tls { throw "test error message"; } } | Should -Throw "test error message"
+        }
+    }
+}
