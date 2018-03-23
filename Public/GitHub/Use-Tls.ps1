588d7601986b3f69af9d543de292e12969c1de4b
Sam Blackburn <sam.blackburn@red-gate.com>
2018-03-23 15:50:50 +0000

    Extract logic to new file

---
+++ Public/GitHub/Use-Tls.ps1
@@ -0,0 +1,27 @@
+
+<#
+        .SYNOPSIS
+        Temporarily sets security protocol to use TLS 1.2
+        .DESCRIPTION
+        Sets the encryption provider to use TLS 1.2, putting it back again once the given code block has exited or thrown
+        .PARAMETER delegate
+        A code block (typically involving one or more web requests) to be executed
+#>
+#requires -Version 2
+function New-PullRequest
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        $delegate
+    )
+    Try {
+        $oldSecurityProtocol = [Net.ServicePointManager]::SecurityProtocol
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        
+        $delegate
+    }
+    Finally {
+        [System.Net.ServicePointManager]::SecurityProtocol = $previousProtocols
+    }
+}
