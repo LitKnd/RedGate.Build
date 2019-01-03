80c5792c12ff766295d2d05523b4a74fed0cf738
Mark Raymond <mark.raymond@red-gate.com>
2019-01-03 15:01:29 +0000

    Add test for WiX bootstrapper AssemblyInfo.cs.

--- Tests/Rewrite-AssemblyInfo.Tests.ps1
+++ Tests/Rewrite-AssemblyInfo.Tests.ps1
@@ -148,4 +148,75 @@ using System.Windows;
             $actualOutput | Should Be $expectedOutput
         }
     }
+    Context 'Default AssemblyInfo.cs for WiX bootstrapper projects' {
+        $initialAssemblyInfo = @"
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using RedGate.SqlClone.Installer.BootstrapperApplication;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RedGate.SqlClone.Installer.BootstrapperApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RedGate.SqlClone.Installer.BootstrapperApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f00e5a4d-9bf7-49e1-9c39-e51045904d45")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: BootstrapperApplication(typeof(NoUiBootstrapper))]
+"@
+        $expectedOutput = @"
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using RedGate.SqlClone.Installer.BootstrapperApplication;
+
+[assembly: AssemblyTitle("RedGate.SqlClone.Installer.BootstrapperApplication")]
+[assembly: AssemblyCompany("Red Gate Software Ltd")]
+[assembly: AssemblyProduct("SQL Clone")]
+[assembly: AssemblyCopyright("Copyright © Red Gate Software Ltd 2019")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f00e5a4d-9bf7-49e1-9c39-e51045904d45")]
+
+[assembly: BootstrapperApplication(typeof(NoUiBootstrapper))]
+
+[assembly: AssemblyVersion("1.2.3.456")]
+[assembly: AssemblyFileVersion("1.2.3.456")]
+
+"@
+        It 'Should rewrite minimal AssemblyInfo.cs' {
+            $filename = (New-TemporaryFile).FullName
+            $initialAssemblyInfo | Out-File $filename -Encoding UTF8
+            Rewrite-AssemblyInfo -ProjectName 'RedGate.SqlClone.Installer.BootstrapperApplication' -ProductName 'SQL Clone' -RootNamespace 'RedGate.SqlClone.Installer.BootstrapperApplication' -AssemblyInfoPath $filename -Version '1.2.3.456' -Year '2019'
+            $actualOutput = Get-Content $filename -Raw -Encoding UTF8
+            $actualOutput | Should Be $expectedOutput
+        }
+    }
 }
